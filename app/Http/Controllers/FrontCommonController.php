e39224b957cea79c5b251bb22d431954cdf46419
Ronald <ronald.jian@gmail.com>
2015-05-28 22:42:40 +0800

    add a front common controller

---
+++ app/Http/Controllers/FrontCommonController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Http\Controllers;
+
+use Sharenjoy\Cmsharenjoy\Http\Controllers\FrontBaseController;
+
+class FrontCommonController extends FrontBaseController {
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+}
