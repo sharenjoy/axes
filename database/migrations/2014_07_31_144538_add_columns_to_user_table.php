1b034b72d49a8b2bd171b3868b45106a4e143ce6
Ronald <ronald.jian@gmail.com>
2016-08-08 14:50:35 +0800

    modify user table schema

--- database/migrations/2014_07_31_144538_add_columns_to_user_table.php
+++ database/migrations/2014_07_31_144538_add_columns_to_user_table.php
@@ -17,6 +17,11 @@ class AddColumnsToUserTable extends Migration {
 		    $table->string('phone')->after('password');
 		    $table->string('avatar')->after('phone');
 		    $table->text('description')->after('avatar');
+		    $table->boolean('activated')->default(0)->after('description');
+			$table->string('activation_code')->nullable()->index()->after('activated');
+			$table->timestamp('activated_at')->nullable()->after('activation_code');
+			$table->timestamp('last_login')->nullable()->after('activated_at');
+			$table->string('reset_password_code')->nullable()->index()->after('last_login');	
 		});
 	}
 
@@ -32,6 +37,11 @@ class AddColumnsToUserTable extends Migration {
 		    $table->dropColumn('phone');
 		    $table->dropColumn('avatar');
 		    $table->dropColumn('description');
+		    $table->dropColumn('activated');
+		    $table->dropColumn('activation_code');
+		    $table->dropColumn('activated_at');
+		    $table->dropColumn('last_login');
+		    $table->dropColumn('reset_password_code');
 		});
 	}
 
