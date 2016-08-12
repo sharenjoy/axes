ce4abc39505023fd579a1016789a24853a51984a
Ronald <ronald.jian@gmail.com>
2016-08-13 04:56:45 +0800

    register an event for controllerAfterAction

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -16,6 +16,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\SomeEvent' => [
             'App\Listeners\EventListener',
         ],
+        'controllerAfterAction' => [
+            'Sharenjoy\Cmsharenjoy\Handlers\Events\ControllerAfterAction@handle',
+        ],
     ];
 
     /**
