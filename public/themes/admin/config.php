7ffb12d1aa505289cd128328eb23cc9f8dc8231a
Ronald <ronald.jian@gmail.com>
2017-05-17 18:19:24 +0800

    modify the place of theme

--- public/themes/admin/config.php
+++ public/themes/admin/config.php
@@ -38,34 +38,34 @@ return array(
             // You can remove this line anytime.
             // $theme->setTitle('Copyright ©  2013 - Laravel.in.th');
 
-            $pkg = 'packages/sharenjoy/cmsharenjoy/';
+            $pkg = url('packages/sharenjoy/cmsharenjoy/');
 
             // CSS
-            $theme->asset()->add('jquery-ui-css', $pkg.'js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css');
-            $theme->asset()->add('entypo'       , $pkg.'css/font-icons/entypo/css/entypo.css');
-            $theme->asset()->add('font-awesome' , $pkg.'css/font-icons/font-awesome/css/font-awesome.min.css');
-            $theme->asset()->add('bootstrap-css', $pkg.'css/bootstrap.css');
-            $theme->asset()->add('neon-core'    , $pkg.'css/neon-core.css');
-            $theme->asset()->add('neon-theme'   , $pkg.'css/neon-theme.css');
-            $theme->asset()->add('neon-forms'   , $pkg.'css/neon-forms.css');
-            $theme->asset()->add('white'        , $pkg.'css/skins/white.css');
-            $theme->asset()->add('animate'      , $pkg.'css/animate.min.css');
-            $theme->asset()->add('custom-css'   , $pkg.'css/sharenjoy/custom.css');
+            $theme->asset()->add('jquery-ui-css', $pkg.'/js/jquery-ui/css/no-theme/jquery-ui-1.10.3.custom.min.css');
+            $theme->asset()->add('entypo'       , $pkg.'/css/font-icons/entypo/css/entypo.css');
+            $theme->asset()->add('font-awesome' , $pkg.'/css/font-icons/font-awesome/css/font-awesome.min.css');
+            $theme->asset()->add('bootstrap-css', $pkg.'/css/bootstrap.css');
+            $theme->asset()->add('neon-core'    , $pkg.'/css/neon-core.css');
+            $theme->asset()->add('neon-theme'   , $pkg.'/css/neon-theme.css');
+            $theme->asset()->add('neon-forms'   , $pkg.'/css/neon-forms.css');
+            $theme->asset()->add('white'        , $pkg.'/css/skins/white.css');
+            $theme->asset()->add('animate'      , $pkg.'/css/animate.min.css');
+            $theme->asset()->add('custom-css'   , $pkg.'/css/sharenjoy/custom.css');
 
             // Javascript
-            $theme->asset()->add('jquery'      , $pkg.'js/jquery-1.11.0.min.js');
-            $theme->asset()->add('gsap'        , $pkg.'js/gsap/main-gsap.js');
-            $theme->asset()->add('jquery-ui-js', $pkg.'js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js');
-            $theme->asset()->add('bootstrap'   , $pkg.'js/bootstrap.js');
-            $theme->asset()->add('joinable'    , $pkg.'js/joinable.js');
-            $theme->asset()->add('resizeable'  , $pkg.'js/resizeable.js');
-            $theme->asset()->add('neon-api'    , $pkg.'js/neon-api.js');
-            $theme->asset()->add('toastr'      , $pkg.'js/toastr.js');
-            $theme->asset()->add('neon-custom' , $pkg.'js/neon-custom.js');
-            $theme->asset()->add('vue-js'      , $pkg.'js/vue/1.0.26/vue.js');
-            // $theme->asset()->add('vue-js'      , $pkg.'js/vue/1.0.26/vue.min.js');
-            $theme->asset()->add('vue-resource-js', $pkg.'js/vue-resource/1.0.3/vue-resource.min.js');
-            $theme->asset()->add('custom-js'   , $pkg.'js/sharenjoy/custom.js');
+            $theme->asset()->add('jquery'      , $pkg.'/js/jquery-1.11.0.min.js');
+            $theme->asset()->add('gsap'        , $pkg.'/js/gsap/main-gsap.js');
+            $theme->asset()->add('jquery-ui-js', $pkg.'/js/jquery-ui/js/jquery-ui-1.10.3.minimal.min.js');
+            $theme->asset()->add('bootstrap'   , $pkg.'/js/bootstrap.js');
+            $theme->asset()->add('joinable'    , $pkg.'/js/joinable.js');
+            $theme->asset()->add('resizeable'  , $pkg.'/js/resizeable.js');
+            $theme->asset()->add('neon-api'    , $pkg.'/js/neon-api.js');
+            $theme->asset()->add('toastr'      , $pkg.'/js/toastr.js');
+            $theme->asset()->add('neon-custom' , $pkg.'/js/neon-custom.js');
+            $theme->asset()->add('vue-js'      , $pkg.'/js/vue/1.0.26/vue.js');
+            // $theme->asset()->add('vue-js'      , $pkg.'/js/vue/1.0.26/vue.min.js');
+            $theme->asset()->add('vue-resource-js', $pkg.'/js/vue-resource/1.0.3/vue-resource.min.js');
+            $theme->asset()->add('custom-js'   , $pkg.'/js/sharenjoy/custom.js');
 
             // Breadcrumb template.
             // $theme->breadcrumb()->setTemplate('
