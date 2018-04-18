16b72ddeb9344ca7504eea8b93f5fe74522c4595
Ronald <ronald.jian@gmail.com>
2018-04-18 16:53:43 +0800

    add some traits

---
+++ app/Http/Traits/AllRoadsLeadToRome.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Traits;
+
+use DB;
+use App\Modules\Order\Order;
+
+trait AllRoadsLeadToRome
+{
+    protected function goRome()
+    {
+        //
+    }
+}
---
+++ app/Http/Traits/DashboardTrait.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Traits;
+
+use DB;
+use App\Modules\Order\Order;
+use App\Modules\Order\OrderAsk;
+
+trait DashboardTrait
+{
+    protected function goDashboard()
+    {
+        //
+    }
+}
